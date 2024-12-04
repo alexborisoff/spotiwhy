aeb534661d3985591e2a317b19da2ee5e10482e8
First Last <borisov.sasha1999@yandex.by>
2024-12-04 20:34:25 +0300

    Renaming SearchInput Component to SearchBar Component and updating Loader style

--- src/components/UI/SearchBar/SearchBar.tsx
+++ src/components/UI/SearchBar/SearchBar.tsx
@@ -1,14 +1,14 @@
 import { ChangeEvent } from 'react';
-import cl from './SearchInput.module.scss';
+import cl from './SearchBar.module.scss';
 
-interface SearchInputProps {
+interface SearchBarProps {
     value?: string;
     placeholder?: string;
     type?: string;
     onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
-export const SearchInput = ({ type, value, placeholder, onChange }: SearchInputProps) => {
+export const SearchBar = ({ type, value, placeholder, onChange }: SearchBarProps) => {
     return (
         <input
             type={type}
