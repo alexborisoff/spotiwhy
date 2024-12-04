632c1d4d0092100c2f1c2dd0a711ca642108777a
First Last <borisov.sasha1999@yandex.by>
2024-12-04 20:35:10 +0300

    UPD: Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,11 @@
-import { ChangeEvent, useState } from 'react';
-import { SearchInput } from './UI/searchInput/SearchInput';
+import { ChangeEvent, useMemo, useState } from 'react';
+import { SearchBar } from './UI/SearchBar/SearchBar';
 
 export const Header = () => {
+    const [selectedSort, setSelectedSort] = useState<string>('');
     const [search, setSearch] = useState<string>('');
+
+    const sortedAudios = useMemo(() => {}, [selectedSort]);
 
     const changeHandler = (e: ChangeEvent<HTMLInputElement>) => {
         setSearch(e.target.value);
@@ -10,7 +13,11 @@ export const Header = () => {
 
     return (
         <header>
-            <SearchInput value={search} placeholder="What do you want to play?" onChange={changeHandler} />
+            <SearchBar
+                value={search}
+                placeholder="What do you want to play?"
+                onChange={changeHandler}
+            />
         </header>
     );
 };
